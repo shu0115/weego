11f6e72772aab2afd3c40f2daaadc4690683b2be
shu_0115 <raisondetre0115@gmail.com>
2012-04-28 16:39:50 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weego::Application.config.secret_token = '6be619e39709be5b19322e5918c9c33109d0dcd9c69034ff03ca9c4e78778daabe876bf6dcb153b8f277c1bd4b041adf3e47a7b3bff8c15d83fa26de13fdaeb3'
